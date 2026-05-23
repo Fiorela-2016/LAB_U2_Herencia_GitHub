796303fc18a8f0ac2287d3cded0a0faab5a8a69b
Geirel <Geirel@DESKTOP-5H54R8H>
2026-05-23 17:04:35 -0600

    Actividad en clase

---
+++ src/LAB_U2_Herencia_GitHub/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_U2_Herencia_GitHub;
+/**
+ *
+ * @author fiore
+ */
+public class Main {
+    public static void main(String[]args) {
+        
+        System.out.println("Sistema de biblioteca ");
+    }
+}
