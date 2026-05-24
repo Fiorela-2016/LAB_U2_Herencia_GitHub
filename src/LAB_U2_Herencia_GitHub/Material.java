c02ebe30b12505bda1faca57226903625f57bcc7
fiore <fiore@192.168.40.15>
2026-05-24 16:39:13 -0600

    laboratorio 1

--- src/LAB_U2_Herencia_GitHub/Material.java
+++ src/LAB_U2_Herencia_GitHub/Material.java
@@ -4,7 +4,7 @@ package LAB_U2_Herencia_GitHub;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+//Clase Base
 
 /**
  *
